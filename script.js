5487c8b35d7df764875f823f14caf6ca4aee9fba
kranthikumar2728 <129576823+kranthikumar2728@users.noreply.github.com>
2026-02-07 15:34:59 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 let messageIndex = 0;
-
 const messages = [
+    "Are you sure?",
     "Really sure??",
+    "Are you positive?",
     "Pookie please...",
     "Just think about it!",
     "If you say no, I will be really sad...",
@@ -15,22 +16,22 @@ function handleNoClick() {
     const noButton = document.querySelector('#noButton');
     const yesButton = document.querySelector('#yesButton');
     
-    // Change the text of the No button
     noButton.textContent = messages[messageIndex];
     messageIndex = (messageIndex + 1) % messages.length;
 
-    // Make the Yes button grow
+    // Grow the Yes button
     const currentSize = parseFloat(window.getComputedStyle(yesButton).fontSize);
     yesButton.style.fontSize = `${currentSize * 1.5}px`;
-    yesButton.style.padding = `${currentSize * 0.8}px ${currentSize * 1.5}px`;
+    yesButton.style.padding = '20px 40px';
 }
 
 function handleYesClick() {
-    // Redirect to a success page or change the content
-    document.querySelector('.container').innerHTML = `
-        <h1 class="header_text">Knew you would say yes!</h1>
+    const container = document.querySelector('.container');
+    // Switch to the second GIF (img3.gif)
+    container.innerHTML = `
+        <h1 class="header_text">Knew you would say yes! ❤️</h1>
         <div class="gif_container">
-            <img src="https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExNHJndnBqZ3ZqZ3ZqZ3ZqZ3ZqZ3ZqZ3ZqZ3ZqZ3ZqZ3ZqZ3ZqJnBvPT1hJmVwPXYxX2ludGVybmFsX2dpZl9ieV9pZCZjdD1n/MDJ9IbxxvDUQM/giphy.gif" alt="Cute Hug">
+            <img src="img3.gif" alt="Hugs" style="height: 250px; border-radius: 15px;">
         </div>
     `;
 }
